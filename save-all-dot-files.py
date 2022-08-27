fbfee855e7a687f6687af563949f6d5272f10196
Calvin Cramer <Calvin.Cramer@windriver.com>
2022-08-26 23:25:41 -0500

    Changes

--- save-all-dot-files.py
+++ save-all-dot-files.py
@@ -1,59 +1,56 @@
 #!/usr/bin/python3
+
+# Copy config files to a folder
+# TODO: handle multiple files with same name
+
 import os.path
-from os.path import dirname, realpath, join, expanduser
+from os.path import dirname, realpath, join, expanduser, basename
 from shutil import copy2
 from pathlib import Path
 
 ARCHIVE_DIR = join(dirname(realpath(__file__)), 'dot-files')
 
-# Add the files here you want saved:
-PATHS_DICT = {
-    '.bash_aliases':    '~/.bash_aliases',
-    '.bash_aliases_wr': '~/.bash_aliases_wr',
-    '.bash_profile':    '~/.bash_profile',
-    '.bashrc':          '~/.bashrc',
-    '.bashrc_wr':       '~/.bashrc_wr',
-    '.gdbinit':         '~/.gdbinit',
-    '.gitconfig':       '~/.gitconfig',
-    '.inputrc':         '~/.inputrc',
-    '.plan':            '~/.plan',
-    '.profile':         '~/.profile',
-    '.rhosts':          '~/.rhosts',
-    '.selected_editor': '~/.selected_editor',
-    '.tmux.conf':       '~/.tmux.conf',
-    '.tmux_dark.conf':  '~/.tmux_dark.conf',
-    '.tmux_light.conf':  '~/.tmux_light.conf',
-    '.toprc':           '~/.toprc',
-    '.vimrc':           '~/.vimrc',
-    '.Xresources':      '~/.Xresources',
-    '.xscreensaver':    '~/.xscreensaver',
-    '.xstartup':        '~/.vnc/xstartup',
-    'skin.yml':         '~/.config/k9s/skin.yml',
-}
-# ~/.config/
-# ~/.private/
+PATHS = [
+    '~/.bash_aliases',
+    '~/.bash_aliases_wr',
+    '~/.bash_profile',
+    '~/.bashrc',
+    '~/.bashrc_wr',
+    '~/.gdbinit',
+    '~/.gitconfig',
+    '~/.inputrc',
+    '~/.plan',
+    '~/.profile',
+    '~/.rhosts',
+    '~/.selected_editor',
+    '~/.tmux.conf',
+    '~/.tmux_dark.conf',
+    '~/.tmux_light.conf',
+    '~/.toprc',
+    '~/.vimrc',
+    '~/.Xresources',
+    '~/.xscreensaver',
+    '~/.vnc/xstartup',
+    '~/.config/k9s/skin.yml',
+]
 
-def copy_to_archive(name, path):
-    # Expand any '~' home tildes
-    path = expanduser(path)
-    # Resolve relative and synlinks in path:
-    path = Path(path).resolve()
-
-    # See if file exists
+def copy_to_archive(name: str, path: str) -> bool:
+    path = expanduser(path)     # Expand any '~' home tildes
+    path = Path(path).resolve() # Resolve relative and synlinks in path:
+    save_name = path.replace(os.path.sep, '|||')    # Preserve path info
     if not os.path.exists(path):
         print(f'Does not exist: {name} -> {path}')
-        return
+        return False
     if not os.path.isfile(path):
         print(f'Exists, but is not a file: {name} -> {path}')
-        return
-
-    # Copy
+        return False
     copy2(src=path, dst=ARCHIVE_DIR, follow_symlinks=True)
+    return True
 
 
 def main():
-    for name, path in PATHS_DICT.items():
-        copy_to_archive(name, path);
+    for path in PATHS:
+        copy_to_archive(basename(path), path);
 
 
 if __name__ == '__main__':
